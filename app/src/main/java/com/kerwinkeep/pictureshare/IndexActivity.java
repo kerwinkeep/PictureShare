0ab4bdeaa43b0028f66e5f1eed267c013a66c0e7
kerwinkeep <kerwinkeep@gmail.com>
2021-12-26 16:23:38 +0800

    首页页面布局搭建

---
+++ app/src/main/java/com/kerwinkeep/pictureshare/IndexActivity.java
@@ -0,0 +1,52 @@
+package com.kerwinkeep.pictureshare;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.kerwinkeep.pictureshare.databinding.ActivityIndexBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.MediaType;
+
+public class IndexActivity extends AppCompatActivity {
+
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private ActivityIndexBinding binding;
+
+//    private List<picture> pictureList = new ArrayList<>();
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityIndexBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
